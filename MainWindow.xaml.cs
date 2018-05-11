55574a7ea8afa1a79812e14ee41b557132247eb9
Jan Doupal <jan.doupal@avast.com>
2018-05-11 15:58:36 +0200

    + just wpf tease

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace vsprojedit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Object to bind the combobox selections to.
+        private ViewModelString viewModelString = new ViewModelString();
+
+        public MainWindow()
+        {
+            // Set the data context for this window.
+            DataContext = viewModelString;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModelStrings.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace vsprojedit
+{
+    class ViewModelString : INotifyPropertyChanged
+    {
+        /// Need a void constructor in order to use as an object element 
+        /// in the XAML.
+        public ViewModelString()
+        {
+        }
+
+        private string _propsTypeString = "(lib)";
+
+        /// String property used in binding examples.
+        public string PropsTypeString
+        {
+            get { return _propsTypeString; }
+            set
+            {
+                if(_propsTypeString != value)
+                {
+                    _propsTypeString = value;
+                    NotifyPropertyChanged("PropsTypeString");
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        /// Need to implement this interface in order to get data binding
+        /// to work properly.
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
